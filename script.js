2fa5756840142e4adde60390210eb81f83b586cb
Indu Prabha <apindhuece12@gmail.com>
2024-04-11 23:24:37 -0400

    area taken for week 3 is calculated

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ const radius = 5;
 
 //calculating the area of a circle :
 const area = PI * radius * radius ;
-console.log (area)
+console.log (area) // 78.5375
 
 const minSpace = 0.8;
 const initialCount = 20;
  //Area needed for 20 plants in week 1:
  const areaWeek1 = initialCount * minSpace ;
- console.log (areaWeek1)
+ console.log (areaWeek1) // 16
 
  //Calculate maximum number of plants the total area can hold
  const maxPlants = area / minSpace;
- console.log(maxPlants)
+ console.log(maxPlants) // 98.1718
 
  //The plants double in number every week.
  //week 2 : 40 plants
  const areaWeek2 = 40 * 0.8;
- console.log(areaWeek2)
+ console.log(areaWeek2) //32
+
+ //week 3 : 80 plants
+ const areaWeek3 = 80 * 0.8;
+ console.log(areaWeek3) //64
