a2d664931440ba99e7eea450a20883cae7caa2b8
Indu Prabha <apindhuece12@gmail.com>
2024-04-11 23:20:33 -0400

    area for week 2 is calculated

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ const initialCount = 20;
  //Area needed for 20 plants in week 1:
  const areaWeek1 = initialCount * minSpace ;
  console.log (areaWeek1)
+
+ //Calculate maximum number of plants the total area can hold
+ const maxPlants = area / minSpace;
+ console.log(maxPlants)
+
+ //The plants double in number every week.
+ //week 2 : 40 plants
+ const areaWeek2 = 40 * 0.8;
+ console.log(areaWeek2)
