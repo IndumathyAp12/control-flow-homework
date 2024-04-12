9b025951d21f1d887eaeff86e50fd405d1b34477
Indu Prabha <apindhuece12@gmail.com>
2024-04-12 17:43:46 -0400

    part 1 fixed errors and part 2 is started

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const radius = 5;
 
 //calculating the area of a circle :
 const area = PI * radius * radius ;
-console.log (area) // 78.5375
+console.log (area) // 78.5375 sqm
 
 const minSpace = 0.8;
 const initialCount = 20;
@@ -15,36 +15,54 @@ const initialCount = 20;
 
   //Area needed for 20 plants in week 1:
   const areaWeek1 = initialCount * minSpace ;
-  console.log (areaWeek1) // 16
+  console.log (areaWeek1) // 16 sqm
 
  //The plants double in number every week.
  //week 2 : 40 plants-need how much area
  const areaWeek2 = 40 * minSpace;
- console.log(areaWeek2) //32
+ console.log(areaWeek2) //32 sqm
 
  //week 3 : 80 plants-need how much area
- const areaWeek3 = 80 * minSpace;
- console.log(areaWeek3) //64
+ const areaWeek3 =  80 * minSpace;
+ console.log(areaWeek3) //64sqm
 
- //week 4 : 160 plants
- const areaWeek4 = 160 * minSpace;
- console.log(areaWeek4) //128 which is higher than total area of garden
- //so not possible to go into week 4.
+//week 4 : 160 plants - needs
+//128 sqm which is higher than total area of garden
+//so not possible to go into week 4.
  
-//Calculate 80% of the maximum capacity of the garden.
+//Calculate area of 80% of the maximum capacity of the garden.
 const maxLimitofplants = 0.8; //80% of the garden
-const  maxCapacityallowed = maxLimitofplants * area;
-console.log(maxCapacityallowed) // 62.83
+const maxAreaAllowed= maxLimitofplants * area;
+console.log(maxAreaAllowed) // 62.83 sqm
+
+ 
+//Calculate area of 50% of the minimum capacity of the garden.
+const minLimitofplants = 0.5; //50% of the garden
+const minAreaAllowed= minLimitofplants * area;
+console.log(minAreaAllowed) // 39.27 sqm
+
+
+//Calculate plant count for 80% in the garden:
+const totalPlants = maxAreaAllowed / minSpace;
+console.log(totalPlants) // 78.537 plants approximately
+
+//Calculate plant count for 50% in the garden:
+const totalPlants1 = minAreaAllowed / minSpace;
+console.log(totalPlants1)// 49.085 plants
+
+//Calculate plant count for week 3 
 
 
 //Control Flow
+
 //Monitored, if they are growing at an acceptable rate.
 //This condition should be met if the plant count is between 50% and
 //80% of the maximum capacity of the garden after the given number of weeks.
 console.log("========Control Flow=========")
-if (areaWeek3 < area * 0.8){
-    console.log("Plant more plants.");
-} else if (areaWeek3 > area * 0.5 && areaWeek3 < area * 0.8){
+let n = 52; // say week 3 plant count
+if (n < totalPlants1){
+    console.log("Plant more");
+} else if (n > totalPlants1 && n < totalPlants){
     console.log("Just monitor the plants.");
 } else {
     console.log("Prune the Plants");
@@ -60,4 +78,7 @@ if (areaWeek3 < area * 0.8){
 
  const newGardenarea = newPlants * minSpace ;
  console.log(newGardenarea); // 80 sq.meters for week 1 in newgarden.
+
+//calculate space needed for the 100 plants at the end of 10 weeks
+
 
