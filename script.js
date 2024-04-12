3592ac82e8d31d25da9b73c86509719506b141a8
Indu Prabha <apindhuece12@gmail.com>
2024-04-12 00:15:44 -0400

    control flow is calculated

--- script.js
+++ script.js
@@ -25,11 +25,23 @@ const initialCount = 20;
  const areaWeek3 = 80 * 0.8;
  console.log(areaWeek3) //64
 
+ //week 4 : 160 plants
+ const areaWeek4 = 160 * 0.8;
+ console.log(areaWeek4) //128 which is higher than total area of garden
+ //so not possible to go into week 4.
+ 
 //Calculate 80% of the maximum capacity of the garden.
 const maxLimitofplants = 0.8; //80% of the garden
 const  maxCapacityallowed = maxLimitofplants * area;
 console.log(maxCapacityallowed) // 62.83
 
- //Pruned,to stop them from exceeding the capacity of the garden.
-//This condition should be met if the plant count 
-//after the given number of weeks is greater than 
+
+//Control Flow
+console.log("========Control Flow=========")
+if (areaWeek3 < area * 0.8){
+    console.log("Plant more plants.");
+} else if (areaWeek3 > area * 0.5 && areaWeek3 < area * 0.8){
+    console.log("Just monitor the plants.");
+} else {
+    console.log("Prune the Plants");
+}
