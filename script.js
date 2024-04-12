bb2f2cec84374af4e842860c0a9205e0ecb1f195
Indu Prabha <apindhuece12@gmail.com>
2024-04-11 23:08:17 -0400

    area needed for plants in week 1 calculated

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //Part 1: Growing Pains
 const PI = 3.1415;
 const radius = 5;
+
+//calculating the area of a circle :
 const area = PI * radius * radius ;
 console.log (area)
 
+const minSpace = 0.8;
+const initialCount = 20;
+ //Area needed for 20 plants in week 1:
+ const areaWeek1 = initialCount * minSpace ;
+ console.log (areaWeek1)
